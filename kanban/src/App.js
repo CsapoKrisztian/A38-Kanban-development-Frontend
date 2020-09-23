98056de5004f9a9bbc2715aae20476d4fef0e382
noemi <gynoemi33@gmail.com>
2020-09-23 13:20:43 +0200

    Implement Header

--- kanban/src/App.js
+++ kanban/src/App.js
@@ -1,8 +1,18 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Main from "./page/Main";
+import Header from "./layout/Header";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+        <Route exact path="/" component={Main} />
+      </Router>
+    </div>
+  );
 }
 
 export default App;
---
+++ kanban/src/layout/Header.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const NavLinks = styled.div`
+  width = 300px;
+`;
+
+function Header() {
+  return (
+    <React.Fragment>
+      <nav className="navbar navbar-expand-lg navbar-success bg-success">
+        <Link className="navbar-brand text-white" to="/">
+          <b>Kanban</b>Sync
+        </Link>
+        <ul className="navbar-nav mr-auto"></ul>
+        <NavLinks className="row">
+          <Link className="text-white" href="(logout or user page)">
+            <div className="col p-2">
+              <i className="fas fa-power-off"></i>
+            </div>
+          </Link>
+          <Link className="text-white mr-3" href="(filter)">
+            <div className="col p-2">
+              <i className="fas fa-cogs"></i>
+            </div>
+          </Link>
+        </NavLinks>
+      </nav>
+    </React.Fragment>
+  );
+}
+
+export default Header;
