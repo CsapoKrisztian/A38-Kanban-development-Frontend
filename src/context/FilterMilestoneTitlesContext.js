ecde07f9752b64eeaf952fc36691e778e05c199b
Tóth Máté <tothmate911@gmail.com>
2020-11-03 14:56:00 +0100

    make getSavedFIlter in context

--- src/context/FilterMilestoneTitlesContext.js
+++ src/context/FilterMilestoneTitlesContext.js
@@ -1,4 +1,5 @@
 import React, { createContext, useState } from "react";
+import getSavedFilters from "./getSavedFilters";
 
 /**
  * Selected filters are stored in this context, so all of the
@@ -7,15 +8,6 @@ import React, { createContext, useState } from "react";
 export const FilterMilestoneTitlesContext = createContext();
 
 export const FilterMilestoneTitlesProvider = (props) => {
-  const getSavedFilters = (localStorageKey) => {
-    let filterString = localStorage.getItem(localStorageKey);
-    if (filterString === null || filterString === '') {
-      return [];
-    } else {
-      return filterString.split(',');
-    }
-  }
-
   const [milestoneTitles, setMilestoneTitles] = useState(getSavedFilters("milestoneTitles"));
 
   return (
