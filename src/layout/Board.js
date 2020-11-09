04dfa2a44aedbb580ef0e323e6a5526956f6d9da
Tóth Máté <tothmate911@gmail.com>
2020-11-09 17:55:28 +0100

    get rid of KanbanBoard.js

--- src/layout/Board.js
+++ src/layout/Board.js
@@ -1,7 +1,12 @@
 import React, { useContext } from "react";
 import Loading from "../components/reuseables/Loading";
-import KanbanTable from "./KanbanTable";
+import styled from "styled-components";
 import { StatusContext } from "../context/StatusContext";
+
+const ScrollWrapper = styled.div`
+  white-space: nowrap;
+  overflow-x: auto;
+`;
 
 /**
  * Fetches statuses and renders KanbanTable
@@ -16,12 +21,33 @@ function Board(props) {
    */
   if (!statusesAreLoading && statuses) {
     content = (
-      <React.Fragment>
-        <KanbanTable statuses={statuses} tableBody={props.tableBody}/>
-      </React.Fragment>
+        <ScrollWrapper>
+          <div className="container-fluid pt-9 pl-3 pr-3 pb-3">
+            <div className="table-responsive-sm">
+              <table className="table table-sm" id="board">
+                <thead
+                // Table header with the statuses
+                >
+                  <tr>
+                    <th className="col"></th>
+                    {statuses.map((status, index) => (
+                      <th key={index} className="col">
+                        {status}
+                      </th>
+                    ))}
+                  </tr>
+                </thead>
+                <tbody
+                // Table body is returned by RenderIssues
+                >
+                  {props.tableBody}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </ScrollWrapper>
     );
   }
-
   return content;
 }
 
